1d5bb4ea304483e8bace24dcfdc90c1f3b2d0710
A S <117680324+singh-an-1008@users.noreply.github.com>
2023-06-24 17:07:52 -0400

    Update Testing_ground.py

--- Testing_ground.py
+++ Testing_ground.py
@@ -1,13 +1,1 @@
-def inout():
-    try:
-        s=input("enter your choice:\n")
-        d=s*s
-        try:
-            def bongo(inout()):
-            c=inout()*7   
-            return c
-        except Exception as e:
-            print("Can you have a look into th eerror which is :\n", str(e))
-    except Exception as e:
-        print("An error has occuered:\n")
-bongo()
+done:
