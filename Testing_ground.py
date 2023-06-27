f0cdb43e5fb07fbff4f11d58e6ec40be0b2d1b1c
Singh.an <singh.an.1008@gmail.com>
2023-06-27 01:21:27 -0400

    jn27

--- Testing_ground.py
+++ Testing_ground.py
@@ -3,7 +3,7 @@ logging.basicConfig(filename='Project.logs', level=logging.DEBUG, format='%(asct
 try: 
     
     def res(name):
-        logging.info("Hello",name)
+        print("Hello",name)
 except Exception as e:
     logging.error("An error has occured:\n")
     logging.exception("This has been cause due to", str(e))
@@ -24,21 +24,94 @@ def call_greet():
     greet(name)
 
 call_greet()
-## Rest next daydsknlkgs
+## python function to shut down your system
 
-## Doing things/ testing g
+import os
 
-def hello():
-    print("Hwllo Buddy how are you:")
-hello()
+def shutdown_system():
+    try:
+        os.system("shutdown /s /t 0")
+    except Exception as e:
+        print("An error occurred while shutting down the system:", str(e))
+        
+## python function to print the system configuration
+import logging
+logging.basicConfig(filename="Project.logs", level=logging.DEBUG, format='%(asctime)s %(message)s %(output)s')
+import platform
+try:
+    def sys_config():
+        system=platform.system()
+        node=platform.node()
+        processor=platform.processor()
+        architecture=platform.architecture()
+        print("system:",system)
+        print("node:",node)
+        print("processor:",processor)
+        print("architecture:",architecture)
+except Exception as e:
+    logging.error("An exception has occured")
+    logging.exception("This has arised due to:\n", str(e))
+sys_config()     
+
+## function to write and access an email
+import smtplib
+import logging
+from email.message import EmailMessage
+logging.basicConfig(filename="Project.logs", level=logging.INFO, format='%(asctime)s %(message)s %(output)s')
+def send_mail(s_mail,r_mail,sub, smtp_server,smtp_port,message):
+    try:
+        mail=EmailMessage()
+        mail['From']=s_mail
+        mail['To']=r_mail
+        mail['Subject']=sub
+        mail.set_content(message)
+        
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.send_message(email)
+        logging.info("the email was shot with success")
+    except Exception as e:
+        logging.error("There is some error")
+        logging.exception("This has been due to:\n", str(e))
+send_mail()          
+    
+    
+    
+    
 
 
-def hunt():
-    print("test")
-hunt()
-print("Demo")
-print("Demo success rate")
-print("Go on")
+def send_email(sender_email, receiver_email, subject, message, smtp_server, smtp_port):
+    logging.basicConfig(filename='email.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+    try:
+        # Create an instance of EmailMessage
+        email = EmailMessage()
+        email['From'] = sender_email
+        email['To'] = receiver_email
+        email['Subject'] = subject
+        email.set_content(message)
 
+        # Connect to the SMTP server
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Log in to the SMTP server if authentication is required
+            # server.login(smtp_username, smtp_password)
 
+            # Send the email
+            server.send_message(email)
+
+        logging.info('Email sent successfully.')
+    except Exception as e:
+        logging.error(f'Error sending email: {str(e)}')
+
+# Example usage
+sender_email = 'your_email@example.com'
+receiver_email = 'recipient@example.com'
+subject = 'Hello, World!'
+message = 'This is a test email sent from Python.'
+smtp_server = 'smtp.example.com'
+smtp_port = 587
+
+send_email(sender_email, receiver_email, subject, message, smtp_server, smtp_port)
+
+    
+   
+        
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,24 @@
 def hello():
     print("Hwllo Buddy how are you:")
 hello()
+print("Hello Hunt")
+
+import smtplib
+import logging
+from email.message import EmailMessage
+logging.basicConfig(filename="Project.logs", level=logging.INFO, format='%(asctime)s %(message)s %(output)s')
+def send_mail(s_mail,r_mail,sub, smtp_server,smtp_port,message):
+    try:
+        mail=EmailMessage()
+        mail['From']=s_mail
+        mail['To']=r_mail
+        mail['Subject']=sub
+        mail.set_content(message)
+        
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.send_message(mail)
+        logging.info("the mail was shot with success")
+    except Exception as e:
+        logging.error("There is some error")
+        logging.exception("This has been due to:\n", str(e))
+send_mail() 
