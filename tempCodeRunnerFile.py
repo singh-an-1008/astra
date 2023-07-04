55f43e9b34da049982b5fb57572a46ace176ae43
Singh.an <singh.an.1008@gmail.com>
2023-07-03 23:20:35 -0400

    july3

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -22,3 +22,25 @@ def send_mail(s_mail,r_mail,sub, smtp_server,smtp_port,message):
         logging.error("There is some error")
         logging.exception("This has been due to:\n", str(e))
 send_mail() 
+## python program to read the index no
+print(" Hello World :/n"*4)
+def loop():
+    s=input("enter your choice:/n")
+    a=0
+    while a<=4:
+        a=a+1
+        print(s)
+loop()
+[ print("tri") for x in range(4)]
+[ "tri" for x in range(4)]
+def index():
+    try:
+        s=input("Enter a choice of yours:\n")
+        for i in enumerate(s):
+            print(i)
+    except Exception as e:
+        print("The error has been due to", str(e))
+index()
+###
+x=(lambda x: x ** 3, 2, 3)
+x
